193cfd4a80d8e756b63a8dbf65cc765c22f0f81c
qvanphong <qvanphong@gmail.com>
2025-02-24 19:59:20 +0700

    fix: change to new ancarat page

--- src/config.js
+++ src/config.js
@@ -6,5 +6,5 @@ module.exports = {
   PRECIOUS_FUND_GOLD_FILE: path.join(__dirname, "../gold_data.json"),
   PRECIOUS_FUND_SILVER_FILE: path.join(__dirname, "../silver_data.json"),
   FMARKET_API_URL: "https://api.fmarket.vn/res/products/filter",
-  ANC_URL: "https://ancarat.com/bac-mieng",
+  ANC_URL: "https://ancarat.com/bac-ancarat",
 };
